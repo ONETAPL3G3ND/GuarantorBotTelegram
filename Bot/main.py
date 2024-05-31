c3e9a739e1d82a4cdaa6bea2707346784c0aa949
Sergii Rudniev <100538048+ONETAPL3G3ND@users.noreply.github.com>
2024-05-31 22:40:27 +0200

    Add files via upload

--- Bot/main.py
+++ Bot/main.py
@@ -1,2 +1,17 @@
 import aiogram
+import logging
+import config
+
+
+logging.basicConfig(
+    level=config.LOGGER_LVL,
+    handlers=[
+        logging.FileHandler(config.LOGGER_NAME_OF_FILE_LOG),
+        logging.StreamHandler(),
+    ],
+    format='[%(asctime)s] - [%(name)s] - [%(levelname)s] - %(message)s',
+)
+
+logger = logging.getLogger(__name__)
+
 
